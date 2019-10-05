110c64681110bff0b79cbaa303a6d631a3cded75
Xiong Ben <xiongben1993@gmail.com>
2019-10-05 23:06:24 +0800

    [add] pymysql

---
+++ dao/base_dao.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import pymysql
+import configparser
+
+class BaseDao(object):
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini', encoding='utf-8')
+
+        host = self.config['db']['host']
+        user = self.config['db']['user']
+        port = self.config.getint('db','port')
+        password = self.config['db']['password']
+        database = self.config['db']['database']
+        charset = self.config['db']['charset']
+
+        self.conn = pymysql.connect(
+            host = host,
+            user = user,
+            port = port,
+            password = password,
+            database = database,
+            charset = charset
+        )
+
+    def close(self):
+        self.conn.close()
